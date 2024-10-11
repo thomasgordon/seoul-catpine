208bbb82e037e842359fce8de8a6c533da1a7a40
thomasgordon <tomandrewgordon@gmail.com>
2024-10-11 14:55:33 +0100

    repo init

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Seoul CatPine",
+	colors = {
+		foreground = "#f5e0dc",
+		background = "#4b4b4b",
+		cursor_bg = "#f5e0dc",
+		cursor_fg = "#4b4b4b",
+		cursor_border = "#f5e0dc",
+		selection_bg = "#595959",
+		selection_fg = "#f5e0dc",
+
+		ansi = { "#4b4b4b", "#e81050", "#d78787", "#baaaa7", "#9ccfd8", "#c4a7e7", "#e0def4", "#f5e0dc" },
+		brights = { "#595959", "#e86310", "#e1ceca", "#cebcb9", "#718bb6", "#c4a7e7", "#f5e0dc", "#949494" },
+	},
+}
